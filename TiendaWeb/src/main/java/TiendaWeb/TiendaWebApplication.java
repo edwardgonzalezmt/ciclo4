ed214e20d86de11022f2a28e045c7b5dc0be0da3
Andres Gonzalez <edward.gonzalez1.mt@correo.usa.edu.co>
2021-12-02 20:39:04 -0500

    andres
    reto2

--- TiendaWeb/src/main/java/TiendaWeb/TiendaWebApplication.java
+++ TiendaWeb/src/main/java/TiendaWeb/TiendaWebApplication.java
@@ -1,13 +1,29 @@
 package TiendaWeb;
 
+import TiendaWeb.Interface.InterfaceClothes;
+import TiendaWeb.Interface.InterfaceUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
-public class TiendaWebApplication {
-
-	public static void main(String[] args) {
+public class TiendaWebApplication implements CommandLineRunner {
+@Autowired
+    private InterfaceClothes interfaceClothes;
+    @Autowired
+    private InterfaceUser interfaceUser;
+    
+    public static void main(String[] args) {
 		SpringApplication.run(TiendaWebApplication.class, args);
 	}
+          @Override
+    public void run(String... args) throws Exception {
+        interfaceClothes.deleteAll();
+        interfaceUser.deleteAll();
+    }
+	
 
 }
